41880aa4e8bfe532bda2b09073272c206a8115db
Пользователь <p_filonchyk@mail.ru>
2020-11-19 20:08:15 +0100

    first commit

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-/*
-Задача сложить все цифры дня рождения формата "1983-14-18"
-1+9+8+3  +  1+4  +  1+8 = 35 = 8
-Вопрос во вложенном цикле (не подсчитывается сумма allSum = 0)
-И возможно этот код упростить?
-Ответь по возможности
-*/
-
 function sumNum(date) {
     let arrDate = date.split("-")
-            console.log(arrDate)
-        allSum = 0
-        console.log(allSum)   
+    allSum = []
     for (let i=0; i<=arrDate.length-1; i++){
-            let way = arrDate[i].split("")
-                console.log(way)                   
-            let numWay = way.map(function (item) {   //let numWay = Number(way1) или +
+            let way = arrDate[i].split("")                 
+            let numWay = way.map(function (item) { 
                 return parseFloat(item);
             });
-                console.log(numWay)
             let sumWay = 0
-           
         for(k=0; k<=numWay.length-1; k++){
             sumWay += numWay[k]
         }
-            console.log(sumWay)
-        allSum += sumWay
+            allSum.push(sumWay)
     }
+    let total = allSum.reduce((a, b) => a + b)
+    let totalArr = [total]
+    let totalAll = (""+totalArr).split("")
+        .map(item => Number(item))
+        .reduce((a, b) => a + b)
+    if(totalAll>=10){
+        let totalMax = (""+totalAll).split("")
+        .map(item => Number(item))
+        .reduce((a, b) => a + b) 
+        return totalMax
+        }
+    console.log(totalAll)
+    return totalAll
 }
 sumNum("1983-14-18")
+
